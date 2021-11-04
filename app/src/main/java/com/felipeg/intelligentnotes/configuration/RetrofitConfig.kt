d8befeb5a88fa85af3fd871a7793d0a4a1566450
Felipe Fabri Galeote <felipe10gale@gmail.com>
2021-11-03 23:09:12 -0300

    Add request interceptor to log http requests and responses
    The logging level is defined to BODY because the system is not in production yet so it is safe to log requests and responses, changed the code in a way that http logger will be enabled by default on the Retrofit builder

--- app/src/main/java/com/felipeg/intelligentnotes/configuration/RetrofitConfig.kt
+++ app/src/main/java/com/felipeg/intelligentnotes/configuration/RetrofitConfig.kt
@@ -7,6 +7,7 @@ import com.felipeg.intelligentnotes.authentication.data.LoginService
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,27 +18,40 @@ class RetrofitConfig {
     fun annotationService(token: String) = makeRetrofitWithToken(token).create(AnnotationService::class.java)
 
     private fun makeRetrofitWithToken(token: String): Retrofit {
+
+        val interceptors : MutableList<Interceptor> = mutableListOf()
+
         val addHeaderInterceptor = Interceptor {
             val request = it.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
             it.proceed(request)
         }
 
-        val client = OkHttpClient().newBuilder().addInterceptor(addHeaderInterceptor).build()
+        interceptors.add(addHeaderInterceptor)
 
-        return Retrofit.Builder()
-            .client(client)
-            .baseUrl(BuildConfig.SERVER_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        return makeRetrofitBuilder(interceptors = interceptors)
             .build()
     }
 
     private fun makeBasicRetrofit(): Retrofit {
+        return makeRetrofitBuilder()
+            .build()
+    }
+
+    private fun makeRetrofitBuilder(interceptors: List<Interceptor> = emptyList()): Retrofit.Builder {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val httpClient = OkHttpClient().newBuilder()
+        httpClient.addInterceptor(logging)
+
+        interceptors.forEach {
+            httpClient.addInterceptor(it)
+        }
+
         return Retrofit.Builder()
+            .client(httpClient.build())
             .baseUrl(BuildConfig.SERVER_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
-            .build()
     }
 
 }
